ba5344272a6a7b238580f28e8f04579b1b4ff3ea
Mattias Rundqvist <mattias.rundqvist@icloud.com>
2021-09-18 22:02:59 +0200

    Added absolute paths
    And added fixed default difficulty

--- root/app/entrypoint.sh
+++ root/app/entrypoint.sh
@@ -1,29 +1,13 @@
 #!/bin/sh
 
+echo "[INFO] *** rundqvist/unmineable ***"
+
 CPU_LIMIT=$(($(nproc) * $CPU_LIMIT_PERCENT))
 
-echo "-- Configuration --"
-echo "* CPU limited to to $CPU_LIMIT ($CPU_LIMIT_PERCENT %)"
-
-if [ -z "$DIFFICULTY" ]; then
-    CPU_SINGLE_SPEED=$(cat /proc/cpuinfo | grep "^[c]pu MHz" | head -n1 | awk '{print $4}' | awk -F'.' '{print $1}')
-    CPU_TOTAL_SPEED=$(($CPU_SINGLE_SPEED * $(nproc) / 100 * $CPU_LIMIT_PERCENT))
-
-    DIFFICULTY=$CPU_TOTAL_SPEED
-
-    if [ -z "$DIFFICULTY" ] || [ "$DIFFICULTY" == "0" ]
-    then
-        DIFFICULTY="50000"
-    fi
-
-    echo "* Difficulty set to: $DIFFICULTY (if too many/few shares accepted, please set DIFFICULTY manually)"
-else
-    echo "* Difficulty set to: $DIFFICULTY"
+if [ -z "$DIFFICULTY" ]
+then    
+    DIFFICULTY="50000"
 fi
-
-echo "* Coin: $COIN"
-echo "* Wallet: $WALLET"
-echo "* Worker: $WORKER"
 
 test $DONATE -eq $DONATE &>/dev/null
 
@@ -31,15 +15,21 @@ if [ $? -ne 0 ] || [ $DONATE -gt 99 ]
 then
     DONATE=1
 fi
-echo "* Donate: $DONATE%"
 
-echo "-- Starting miner --"
+echo "[INFO] CPU limited to to $CPU_LIMIT ($CPU_LIMIT_PERCENT %)"
+echo "[INFO] Difficulty set to: $DIFFICULTY (if too many/few shares accepted, please adjust difficulty)"
+echo "[INFO] Coin: $COIN"
+echo "[INFO] Wallet: $WALLET"
+echo "[INFO] Worker: $WORKER"
+echo "[INFO] Donate: $DONATE%"
+
+echo "[INFO] Configuring miner"
 
 cp -f /app/config.org.json /app/config.json
 
 if [ -z "$COIN" ] || [ -z "$WALLET" ]
 then
-    echo "WARNING: Coin or Wallet not configured. Starting demo."
+    echo "[WARN] Coin or Wallet not configured. Starting demo."
 
     sed -i "s/rx.unmineable.com:3333/stratum+ssl:\/\/donatexmr.duckdns.org:20000/g" /app/config.json
     sed -i "s/COIN:WALLET.WORKER+DIFFICULTY#l7nr-nohw/docker/g" /app/config.json
@@ -52,7 +42,9 @@ fi
 
 sed -i "s/DONATE/$DONATE/g" /app/config.json
 
-xmrig -c /app/config.json &
+echo "[INFO] Starting miner"
+
+/usr/sbin/xmrig -c /app/config.json &
 sleep 3
 
-cpulimit -l $CPU_LIMIT -p $(pidof xmrig) -z
+/usr/bin/cpulimit -l $CPU_LIMIT -p $(pidof xmrig) -z
